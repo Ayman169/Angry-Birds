f51be2969d06412cdbe371b4a3adaf0f233649b9
Sayeda Ayman <74244510+Ayman169@users.noreply.github.com>
2020-12-01 16:56:16 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -1,28 +1,29 @@
-class Log {
-    constructor(x,y,height,angle){
+class Bird {
+    constructor(x,y){
 
        var abc = {
            restitution:1,
            density:1,
-           friction:2
+           friction:1
        }
 
-        this.body = Bodies.rectangle(x,y,20,height,abc);
-        this.width = 20;
-        this.height = height;
-        Matter.Body.setAngle(this.body,angle);
+        this.body = Bodies.rectangle(x,y,50,50,abc);
+        this.width = 50;
+        this.height = 50;
         World.add(myWorld,this.body);
     }
 
     display(){
 
        var pos = this.body.position;
+       pos.x=mouseX;
+       pos.y=mouseY;
        var angle = this.body.angle;
        push();
        translate(pos.x,pos.y);
        rotate(angle);
 
-       fill("red");
+       fill("blue");
        rectMode(CENTER);
        rect(0,0,this.width,this.height);
        pop();
--- sketch.js
+++ sketch.js
@@ -14,22 +14,39 @@ function setup(){
    
     box = new Box(800,570);
     box2 = new Box(1000,570);
+    box3 = new Box(830,500);
+    box4 = new Box(970,500);
+    box5 = new Box(900,450);
 
     pig = new Pig(900,570);
+    pig2 = new Pig(900,500);
 
-    log = new Log(900,530,300,PI/2)
+    log = new Log(900,530,300,PI/2);
+    log2= new Log(900,480,300,PI/2);
+    log3 = new Log(830,470,150,PI/7);
+    log4=new Log(970,470,150,-PI/7);
+
+    bird = new Bird(200,570)
 
     ground = new Ground(600,590,1200,20);
 }
 
 function draw(){
     background(0);
-   
+  console.log(frameCount) 
 
     Engine.update(myEngine);
     box.display();
     box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
     pig.display();
+    pig2.display();
     log.display();
+    log2.display();
+    log3.display();
+    log4.display();
+    bird.display();
     ground.display();
 }
