90624423f133a6f3305bb0041c13f30b58f80744
Sayeda Ayman <74244510+Ayman169@users.noreply.github.com>
2020-12-08 16:54:05 +0530

    Add files via upload

--- Pig.js
+++ Pig.js
@@ -1,29 +1,7 @@
-class Pig {
+class Pig extends Baseclass{
+
     constructor(x,y){
-
-       var abc = {
-           restitution:1,
-           density:1,
-           friction:1
-       }
-
-        this.body = Bodies.rectangle(x,y,50,50,abc);
-        this.width = 50;
-        this.height = 50;
-        World.add(myWorld,this.body);
-    }
-
-    display(){
-
-       var pos = this.body.position;
-       var angle = this.body.angle;
-       push();
-       translate(pos.x,pos.y);
-       rotate(angle);
-
-       fill("green");
-       rectMode(CENTER);
-       rect(0,0,this.width,this.height);
-       pop();
+      super(x,y,50,50);
+      this.image = loadImage("sprites/enemy.png");
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var myEngine, myWorld;
-var box,ground;
+var box,ground,back;
+
+function preload(){
+  back=loadImage("sprites/bg.png");
+}
 
 function setup(){
     createCanvas(1200,600);
@@ -14,22 +19,46 @@ function setup(){
    
     box = new Box(800,570);
     box2 = new Box(1000,570);
+    box3 = new Box(830,500);
+    box4 = new Box(970,500);
+    box5 = new Box(900,450);
 
     pig = new Pig(900,570);
+    pig2 = new Pig(900,500);
 
-    log = new Log(900,530,300,PI/2)
+    log = new Log(900,530,300,PI/2);
+    log2= new Log(900,480,300,PI/2);
+    log3 = new Log(830,470,150,PI/7);
+    log4=new Log(970,470,150,-PI/7);
+    conlog=new Log(250,200,100,PI/2)
+
+    bird = new Bird(200,570)
 
     ground = new Ground(600,590,1200,20);
+    ground2 = new Ground(200,470,350,250);
+
+    chain = new Chain(bird.body,conlog.body)
 }
 
 function draw(){
-    background(0);
-   
+    background(back);
+  console.log(frameCount) 
 
     Engine.update(myEngine);
     box.display();
     box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
     pig.display();
+    pig2.display();
     log.display();
+    log2.display();
+    log3.display();
+    log4.display();
+    conlog.display();
+    bird.display();
     ground.display();
+    ground2.display();
+    chain.display();
 }
