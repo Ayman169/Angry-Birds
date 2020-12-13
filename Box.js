58ddf4f6f0789aa97abc436074ab50e76bf74086
Sayeda Ayman <74244510+Ayman169@users.noreply.github.com>
2020-12-13 09:51:58 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,29 +1,7 @@
- class Box {
-     constructor(x,y){
+class Box extends Baseclass{
 
-        var abc = {
-            restitution:1,
-            density:1,
-            friction:1
-        }
-
-         this.body = Bodies.rectangle(x,y,70,70,abc);
-         this.width = 70;
-         this.height = 70;
-         World.add(myWorld,this.body);
-     }
-
-     display(){
-
-        var pos = this.body.position;
-        var angle = this.body.angle;
-        push();
-        translate(pos.x,pos.y);
-        rotate(angle);
-
-        fill("yellow");
-        rectMode(CENTER);
-        rect(0,0,this.width,this.height);
-        pop();
-     }
- }
+    constructor(x,y){
+      super(x,y,70,70);
+      this.image = loadImage("sprites/wood1.png");
+    }
+}
